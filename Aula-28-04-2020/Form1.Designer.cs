c4f062ded3b27b208c9be50143e8e552e650ef69
Dante Souza e Souza <lokinmodar@gmail.com>
2020-04-28 22:02:18 -0300

    Codificando funcionalidades

--- Aula-28-04-2020/Form1.Designer.cs
+++ Aula-28-04-2020/Form1.Designer.cs
@@ -34,14 +34,14 @@
             this.btnNumeros = new System.Windows.Forms.Button();
             this.lblSignoTipo = new System.Windows.Forms.Label();
             this.pnlNumSorte = new System.Windows.Forms.Panel();
+            this.lblMega = new System.Windows.Forms.Label();
+            this.lnlNumSorte = new System.Windows.Forms.Label();
             this.pnlNome = new System.Windows.Forms.Panel();
             this.dtpNiver = new System.Windows.Forms.DateTimePicker();
             this.lblDtNasc = new System.Windows.Forms.Label();
             this.lblNome = new System.Windows.Forms.Label();
             this.btnPrever = new System.Windows.Forms.Button();
             this.txtNome = new System.Windows.Forms.TextBox();
-            this.lnlNumSorte = new System.Windows.Forms.Label();
-            this.lblMega = new System.Windows.Forms.Label();
             this.pnlHoroscopo.SuspendLayout();
             this.pnlNumSorte.SuspendLayout();
             this.pnlNome.SuspendLayout();
@@ -53,35 +53,39 @@
             this.pnlHoroscopo.Controls.Add(this.lblSignoDesc);
             this.pnlHoroscopo.Controls.Add(this.btnNumeros);
             this.pnlHoroscopo.Controls.Add(this.lblSignoTipo);
-            this.pnlHoroscopo.Location = new System.Drawing.Point(7, 171);
+            this.pnlHoroscopo.Location = new System.Drawing.Point(8, 196);
+            this.pnlHoroscopo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlHoroscopo.Name = "pnlHoroscopo";
-            this.pnlHoroscopo.Size = new System.Drawing.Size(315, 256);
+            this.pnlHoroscopo.Size = new System.Drawing.Size(420, 252);
             this.pnlHoroscopo.TabIndex = 7;
             // 
             // lblSigno
             // 
             this.lblSigno.AutoSize = true;
-            this.lblSigno.Location = new System.Drawing.Point(18, 10);
+            this.lblSigno.Location = new System.Drawing.Point(24, 12);
+            this.lblSigno.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSigno.Name = "lblSigno";
-            this.lblSigno.Size = new System.Drawing.Size(69, 13);
+            this.lblSigno.Size = new System.Drawing.Size(91, 17);
             this.lblSigno.TabIndex = 14;
             this.lblSigno.Text = "Seu signo é: ";
             // 
             // lblSignoDesc
             // 
             this.lblSignoDesc.AutoSize = true;
-            this.lblSignoDesc.Location = new System.Drawing.Point(18, 44);
-            this.lblSignoDesc.MaximumSize = new System.Drawing.Size(250, 120);
+            this.lblSignoDesc.Location = new System.Drawing.Point(24, 54);
+            this.lblSignoDesc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblSignoDesc.MaximumSize = new System.Drawing.Size(333, 148);
             this.lblSignoDesc.Name = "lblSignoDesc";
-            this.lblSignoDesc.Size = new System.Drawing.Size(55, 13);
+            this.lblSignoDesc.Size = new System.Drawing.Size(71, 17);
             this.lblSignoDesc.TabIndex = 13;
             this.lblSignoDesc.Text = "Descrição";
             // 
             // btnNumeros
             // 
-            this.btnNumeros.Location = new System.Drawing.Point(19, 194);
+            this.btnNumeros.Location = new System.Drawing.Point(27, 203);
+            this.btnNumeros.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnNumeros.Name = "btnNumeros";
-            this.btnNumeros.Size = new System.Drawing.Size(276, 50);
+            this.btnNumeros.Size = new System.Drawing.Size(368, 35);
             this.btnNumeros.TabIndex = 12;
             this.btnNumeros.Text = "Me mostre meus números!";
             this.btnNumeros.UseVisualStyleBackColor = true;
@@ -90,9 +94,10 @@
             // lblSignoTipo
             // 
             this.lblSignoTipo.AutoSize = true;
-            this.lblSignoTipo.Location = new System.Drawing.Point(18, 27);
+            this.lblSignoTipo.Location = new System.Drawing.Point(24, 33);
+            this.lblSignoTipo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblSignoTipo.Name = "lblSignoTipo";
-            this.lblSignoTipo.Size = new System.Drawing.Size(66, 13);
+            this.lblSignoTipo.Size = new System.Drawing.Size(87, 17);
             this.lblSignoTipo.TabIndex = 0;
             this.lblSignoTipo.Text = "Seu signo é ";
             // 
@@ -100,10 +105,32 @@
             // 
             this.pnlNumSorte.Controls.Add(this.lblMega);
             this.pnlNumSorte.Controls.Add(this.lnlNumSorte);
-            this.pnlNumSorte.Location = new System.Drawing.Point(7, 443);
+            this.pnlNumSorte.Location = new System.Drawing.Point(8, 456);
+            this.pnlNumSorte.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlNumSorte.Name = "pnlNumSorte";
-            this.pnlNumSorte.Size = new System.Drawing.Size(315, 97);
+            this.pnlNumSorte.Size = new System.Drawing.Size(420, 119);
             this.pnlNumSorte.TabIndex = 8;
+            // 
+            // lblMega
+            // 
+            this.lblMega.AutoSize = true;
+            this.lblMega.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMega.Location = new System.Drawing.Point(32, 47);
+            this.lblMega.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblMega.Name = "lblMega";
+            this.lblMega.Size = new System.Drawing.Size(0, 25);
+            this.lblMega.TabIndex = 5;
+            this.lblMega.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lnlNumSorte
+            // 
+            this.lnlNumSorte.AutoSize = true;
+            this.lnlNumSorte.Location = new System.Drawing.Point(21, 17);
+            this.lnlNumSorte.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lnlNumSorte.Name = "lnlNumSorte";
+            this.lnlNumSorte.Size = new System.Drawing.Size(186, 17);
+            this.lnlNumSorte.TabIndex = 4;
+            this.lnlNumSorte.Text = "Seus números da sorte são:";
             // 
             // pnlNome
             // 
@@ -112,41 +139,46 @@
             this.pnlNome.Controls.Add(this.lblNome);
             this.pnlNome.Controls.Add(this.btnPrever);
             this.pnlNome.Controls.Add(this.txtNome);
-            this.pnlNome.Location = new System.Drawing.Point(10, 8);
+            this.pnlNome.Location = new System.Drawing.Point(8, 10);
+            this.pnlNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.pnlNome.Name = "pnlNome";
-            this.pnlNome.Size = new System.Drawing.Size(312, 157);
+            this.pnlNome.Size = new System.Drawing.Size(421, 178);
             this.pnlNome.TabIndex = 9;
             // 
             // dtpNiver
             // 
-            this.dtpNiver.Location = new System.Drawing.Point(18, 78);
+            this.dtpNiver.Location = new System.Drawing.Point(24, 96);
+            this.dtpNiver.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.dtpNiver.Name = "dtpNiver";
-            this.dtpNiver.Size = new System.Drawing.Size(274, 20);
+            this.dtpNiver.Size = new System.Drawing.Size(364, 22);
             this.dtpNiver.TabIndex = 14;
             // 
             // lblDtNasc
             // 
             this.lblDtNasc.AutoSize = true;
-            this.lblDtNasc.Location = new System.Drawing.Point(15, 62);
+            this.lblDtNasc.Location = new System.Drawing.Point(20, 76);
+            this.lblDtNasc.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblDtNasc.Name = "lblDtNasc";
-            this.lblDtNasc.Size = new System.Drawing.Size(177, 13);
+            this.lblDtNasc.Size = new System.Drawing.Size(233, 17);
             this.lblDtNasc.TabIndex = 13;
             this.lblDtNasc.Text = "Selecione sua Data de Nascimento:";
             // 
             // lblNome
             // 
             this.lblNome.AutoSize = true;
-            this.lblNome.Location = new System.Drawing.Point(15, 6);
+            this.lblNome.Location = new System.Drawing.Point(20, 7);
+            this.lblNome.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblNome.Name = "lblNome";
-            this.lblNome.Size = new System.Drawing.Size(88, 13);
+            this.lblNome.Size = new System.Drawing.Size(116, 17);
             this.lblNome.TabIndex = 12;
             this.lblNome.Text = "Digite seu Nome:";
             // 
             // btnPrever
             // 
-            this.btnPrever.Location = new System.Drawing.Point(18, 104);
+            this.btnPrever.Location = new System.Drawing.Point(24, 128);
+            this.btnPrever.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.btnPrever.Name = "btnPrever";
-            this.btnPrever.Size = new System.Drawing.Size(276, 50);
+            this.btnPrever.Size = new System.Drawing.Size(368, 43);
             this.btnPrever.TabIndex = 11;
             this.btnPrever.Text = "Prever!";
             this.btnPrever.UseVisualStyleBackColor = true;
@@ -155,39 +187,22 @@
             // txtNome
             // 
             this.txtNome.Font = new System.Drawing.Font("Corbel", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNome.Location = new System.Drawing.Point(18, 22);
+            this.txtNome.Location = new System.Drawing.Point(24, 27);
+            this.txtNome.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.txtNome.Name = "txtNome";
-            this.txtNome.Size = new System.Drawing.Size(274, 27);
+            this.txtNome.Size = new System.Drawing.Size(364, 32);
             this.txtNome.TabIndex = 10;
-            // 
-            // lnlNumSorte
-            // 
-            this.lnlNumSorte.AutoSize = true;
-            this.lnlNumSorte.Location = new System.Drawing.Point(16, 14);
-            this.lnlNumSorte.Name = "lnlNumSorte";
-            this.lnlNumSorte.Size = new System.Drawing.Size(138, 13);
-            this.lnlNumSorte.TabIndex = 4;
-            this.lnlNumSorte.Text = "Seus números da sorte são:";
-            // 
-            // lblMega
-            // 
-            this.lblMega.AutoSize = true;
-            this.lblMega.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblMega.Location = new System.Drawing.Point(24, 38);
-            this.lblMega.Name = "lblMega";
-            this.lblMega.Size = new System.Drawing.Size(0, 20);
-            this.lblMega.TabIndex = 5;
-            this.lblMega.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
             // 
             // FrmPredictor
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(331, 552);
+            this.ClientSize = new System.Drawing.Size(441, 579);
             this.Controls.Add(this.pnlNome);
             this.Controls.Add(this.pnlNumSorte);
             this.Controls.Add(this.pnlHoroscopo);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.MinimizeBox = false;
             this.Name = "FrmPredictor";
--- Aula-28-04-2020/Form1.cs
+++ Aula-28-04-2020/Form1.cs
@@ -19,35 +19,45 @@ namespace Aula_28_04_2020
 
         private void FrmPredictor_Load(object sender, EventArgs e)
         {
-            String mega = GenMega();
-            SetDiaMesData();
+            // Funções executadas quando o formulário é carregado
+
+            // chamando a função que formata o DateTimePicker
+            SetDiaMesData();            
+
+            // Tornando visível o pnlNome
             pnlNome.Visible = true;
+            // Ativando esse painel
             pnlNome.Enabled = true;
+
+            // Escondendo os outros dois Panels
             pnlNumSorte.Visible = false;
             pnlHoroscopo.Visible = false;
         }
 
 
-
-        private static String GenMega() // gera o valor dos 6 números para a pessoa
+        // gera o valor dos 6 números para a pessoa
+        private static String GenMega() 
         {
+            // definindo 6 variáveis int
             int dz1, dz2, dz3, dz4, dz5, dz6;
+
+            // definindo variável pra receber o texto dos 6 números
             String megaNums = "";
 
-            dz1 = GetRandom(0, 59);
-            dz2 = GetRandom(0, 59);
-            dz3 = GetRandom(0, 59);
-            dz4 = GetRandom(0, 59);
-            dz5 = GetRandom(0, 59);
-            dz6 = GetRandom(0, 59);
+            dz1 = GetRandom(01, 60);
+            dz2 = GetRandom(01, 60);
+            dz3 = GetRandom(01, 60);
+            dz4 = GetRandom(01, 60);
+            dz5 = GetRandom(01, 60);
+            dz6 = GetRandom(01, 60);
 
             megaNums = dz1 + " - " + dz2 + " - " + dz3 + " - " + dz4 + " - " + dz5 + " - " + dz6;
             Console.WriteLine(megaNums);
             return megaNums;
         }
 
-
-        private static int GetRandom(int min, int max) // função que cria números de 2 dígitos aleatórios dentro de um intervalo
+        // função que cria números de 2 dígitos aleatórios dentro de um intervalo
+        private static int GetRandom(int min, int max) 
         {
             using (System.Security.Cryptography.RNGCryptoServiceProvider rng = new System.Security.Cryptography.RNGCryptoServiceProvider())
             {
@@ -59,22 +69,108 @@ namespace Aula_28_04_2020
         }
 
 
+
+
+
+
+
+
         private void btnPrever_Click(object sender, EventArgs e)
         {
+            /* Captando o valor do DateTimePicker (data de aniversário) e
+             * jogando numa variável do tipo DateTime */ 
+            DateTime niver = dtpNiver.Value;
+            // testando se estamos captando o valor
+            Console.WriteLine(niver.ToString());
 
-           
+            // Definindo datas iniciais de cada signo
+            DateTime aries = new DateTime(2020, 03, 21);
+            DateTime touro = new DateTime(2020, 04, 21);
+            DateTime gemeos = new DateTime(2020, 05, 21);
+            DateTime cancer = new DateTime(2020, 06, 21);
+            DateTime leao = new DateTime(2020, 07, 23);
+            DateTime virgem = new DateTime(2020, 08, 23);
+            DateTime libra = new DateTime(2020, 09, 23);
+            DateTime escorpiao = new DateTime(2020, 10, 23);
+            DateTime sagitario = new DateTime(2020, 11, 22);
+            DateTime capricornio = new DateTime(2020, 12, 22);
+            DateTime aquario = new DateTime(2021, 01, 20);
+            DateTime peixes = new DateTime(2021, 03, 21);
+
+
+            // Desativando o Panel pnlNome
+            pnlNome.Enabled = false;
+
+            // Escondendo os outros dois Panels
+            pnlHoroscopo.Visible = true;
+            pnlNumSorte.Visible = false;
+
+            // comparando os valores do aniversário com as datas de cada signo
+            if (niver >= aries && niver < touro)
+            {
+                // Captando o texto já existente no lblSigno e adicionando o texto Áries
+                lblSigno.Text = lblSigno.Text + "Áries";
+                lblSignoDesc.Text = "Descrição do Signo... ";
+                lblSignoTipo.Text = lblSignoTipo.Text + "de Fogo.";
+            }
+            else if (niver >= touro && niver < gemeos)
+            {
+                lblSigno.Text = lblSigno.Text + "Touro";
+                lblSignoDesc.Text = "Descrição do Signo... ";
+                lblSignoTipo.Text = lblSignoTipo.Text + "de Terra.";
+            }
+            else if (niver >= gemeos && niver < cancer)
+            {
+                lblSigno.Text = lblSigno.Text + "Gêmeos";
+                lblSignoDesc.Text = "Descrição do Signo... ";
+                lblSignoTipo.Text = lblSignoTipo.Text + "de Ar.";
+            }
+            else if (niver >= cancer && niver < leao)
+            {
+                lblSigno.Text = lblSigno.Text + "Câncer";
+                lblSignoDesc.Text = "Descrição do Signo... ";
+                lblSignoTipo.Text = lblSignoTipo.Text + "de Água.";
+            }
+
         }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
         private void btnNumeros_Click(object sender, EventArgs e)
         {
-
+            //String mega = GenMega();
         }
+
+
+
+
+
+
+
+
+        // formata o uso do DateTimePicker 
         public void SetDiaMesData()
-        {
-            // Set the Format type and the CustomFormat string.
+        {            
+            // determina que o dtpNiver terá um formato customizado
             dtpNiver.Format = DateTimePickerFormat.Custom;
+
+            // infoma qual é esse formato customizado
             dtpNiver.CustomFormat = "dd/MM";
+
+            // faz não deixar selecionar a data manualmente, obrigando a digitação
             dtpNiver.ShowUpDown = true;
         }
     }
